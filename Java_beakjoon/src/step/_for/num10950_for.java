32a61f65a487790f867e71d41ec75a5d0201e658
Jingon-Park <bennp3038@gmail.com>
2020-02-05 16:13:42 +0900

    Create num10950_for.java
    백준_단계_for문_10950번 정답

---
+++ Java_beakjoon/src/step/_for/num10950_for.java
@@ -0,0 +1,21 @@
+package algo;
+import java.util.*;
+public class num10950_for {
+
+	public static void main(String[] args) {
+		int times;
+		Scanner cs = new Scanner(System.in);
+		times = cs.nextInt();
+		int[] members = new int[times];
+		members[0] = cs.nextInt();
+		members[1] = cs.nextInt();
+		System.out.println(members[0]+ members[1]);
+		
+		//for(int i = 1; i <= (times * 2); i = i*2) {
+			
+		//}
+		
+
+	}
+
+}
