d8167780f22a9a1e931afa6729167b6eda42fb3b
Jingon-Park <bennp3038@gmail.com>
2020-03-18 11:29:02 +0900

    Create num10039_practice1.java

---
+++ Java_beakjoon/src/step/_practice/num10039_practice1.java
@@ -0,0 +1,19 @@
+package step._practice;
+import java.util.*;
+public class num10039_practice1 {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		Scanner sc = new Scanner(System.in);
+		for(int num = 5 ; num > 0; num--) {
+			int score = sc.nextInt();
+			if(score < 40) {
+				score = 40;
+			}
+			sum +=score;
+		}
+		System.out.println(sum / 5);
+
+	}
+
+}
